c39898da15a702ba960777a98955866dd47636d1
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-10-25 22:14:24 +0200

    ts to java

--- User.java
+++ User.java
@@ -1,28 +1,9 @@
 import java.util.Date;
+import java.util.ArrayList;
 
-public class User{
+public class User {
     private Date birthdate;
-    private int size;
-    private Photo[] photos;
-    private Address address ; 
-
-
-
-    public int getSize() {
-        return this.size;
-    }
-
-    public Address getAddress(){
-        return this.address;
-    }
-
-    public Date getBirthdate(){
-       return  this.birthdate;
-    }
-
-    public Photo [] getPhotos(){
-        return this.photos;
-    }
-
-
+    private Integer size;
+    private ArrayList<Photo> photos;
+    private Address address;
 }
